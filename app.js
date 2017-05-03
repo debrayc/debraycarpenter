f44a57a4935d2fe8d528d0d0d86d33676d94cd7f
DeBray Carpenter <debraycarpenter@DeBrays-MacBook-Pro.local>
2017-05-03 16:16:13 -0700

    created skeleton app with links and most functionality

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
+
 let path = require('path');
 let express = require('express');
 
 let app = express();
-
-let staticPath = path.join(__dirname, '/public');
+var staticPath = path.join(__dirname, '/public');
 app.use(express.static(staticPath));
 
+app.get('/', (req, res) => {
+  res.send('index.html')
+})
+
+app.get('/blog', (req, res) => {
+  res.sendFile(staticPath + '/blog.html')
+})
+
+app.get('/contact', (req, res) => {
+  res.sendFile(staticPath + '/contact.html')
+})
+
+app.get('http://ifaacademy.eu/wp-content/uploads/2015/12/linkedin-logo-600x612.png', (reg, res) => {
+  res.sendFile('http://ifaacademy.eu/wp-content/uploads/2015/12/linkedin-logo-600x612.png')
+})
+
 app.listen(3000, function() {
-  console.log('listening');
+  console.log();
 });
